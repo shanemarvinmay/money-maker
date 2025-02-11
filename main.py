6524f8a6ffbd8a3a041adeb436968809e24c036e
shanemarvinmay <shanemarvinmay@gmail.com>
2025-02-10 21:16:41 -0800

    Create S&P dataset and preview data.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import yfinance
+import json
+from collections import Counter
 
 def main():
     print("Hello from money-maker!")
@@ -7,3 +9,15 @@ def main():
 if __name__ == "__main__":
     main()
     help(yfinance)
+    with open('snp_500.json', 'r') as f:
+        data = json.load(f)
+    print('Preview S&P JSON Data')
+    for k, l in data.items():
+        print(k, l[:3])
+    print()
+    print('Sector Counts')
+    sector_counts = Counter(data['sectors'])
+    for sector, count in sector_counts.items():
+        print(sector, count)
+    
+
